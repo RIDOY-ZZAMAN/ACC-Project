e5e1ce61d754586e9223fd208c2b9d02b7a7c5a9
RIDOY-ZZAMAN <ridoyzzaman957@outlook.com>
2022-05-28 12:30:05 +0600

    project setup done

---
+++ src/routes/adminRoutes.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const adminRoutes = () => {
+  return <div></div>;
+};
+
+export default adminRoutes;
---
+++ src/views/pages/common/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <h2>Main Body</h2>
+    </div>
+  );
+};
+
+export default Main;
